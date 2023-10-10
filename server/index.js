799b90ff00d8f468ae30305f8948bf5cc0e0c446
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2023-10-11 01:18:32 +0500

    Created a test api route

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRouter from "./routes/user.router.js ";
+
 dotenv.config();
+
+const app = express();
 
 /*  */
 
@@ -15,8 +19,20 @@ mongoose
     console.log(error);
   });
 
-const app = express();
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+/* 
+
+Below is just to see how an API route is made. This is not good practice. We
+have to make a different folder for these API routes. Creating a folder called routes 
+app.get("/", (req, res) => {
+  res.json({
+    message: "What's poppin?",
+  });
+});
+
+*/
+
+app.use("/user", userRouter);
